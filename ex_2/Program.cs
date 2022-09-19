13b69f8650a61935d670aad236f88b49fdaf91a8
AlexeyBolt <boltava.av@gmail.com>
2022-09-19 23:29:31 +0300

    решил 2ю задачу

---
+++ ex_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+Console.Clear();
+
+int number = new Random().Next(100,999);
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int secondDigit = number % 10;
+Console.Write($"{firstDigit}{secondDigit}");
